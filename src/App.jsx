cc82fa840c60271dca1baeee2d25656e34e829de
Prakat Lamichhane <prakatlamichhane@gmail.com>
2025-02-19 08:09:15 +0545

    Added Page folder and Homepage jsx and css file and its basic structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import {HomePage} from './pages/HomePage.jsx';
+
+export default function(){
+  return(
+    <>
+    <HomePage/>
+    </>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import './HomePage.css';
+
+export default function HomePage(){
+    return(
+        <>
+        <div>This Is Home Page</div>
+        </>
+    );
+}
