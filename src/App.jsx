0f3611a2a2dd108f5ecb4449e0e361557c4f5207
prakatlamichhane123 <prakatlamichhane@gmail.com>
2025-04-07 18:57:35 +0545

    Added Routes and ExplorePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar.jsx';
 import HomePage from './pages/HomePage.jsx';
-
+import { Routes,Route } from 'react-router-dom';
 export default function(){
   return(
     <>
-    <HomePage/>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+    </Routes>
+    
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 
   </StrictMode>,
 )
