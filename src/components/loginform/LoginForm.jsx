58087bb436bec7c9d9ecda689a2df93d90feb8d0
prakatlamichhane123 <prakatlamichhane@gmail.com>
2025-04-17 00:50:11 +0545

    Login Page modification

--- src/components/loginform/LoginForm.jsx
+++ src/components/loginform/LoginForm.jsx
@@ -3,8 +3,27 @@ import './LoginForm.css'
 export default function LoginForm(prop) {
     return (
         <>
-            <div className="login-forum-container">
-                
+            <div className="login-form-container">
+                <div className="logo">LOGO!</div>
+                <div className="welcome-text-container">
+                    <span className="welcome-first">Welcome Back</span>
+                    <span className="welcome-second">Please Login To Your Account</span>
+                </div>
+
+                <div className="login-area">
+                    <input type="text" className='username-box login-input-field' name="username-box" id="username-box" placeholder='Username/Phone'/>
+
+                    <input type="text" className='password-box login-input-field' name="password-box" id="password-box" placeholder='Password'/>
+                    
+                    <a href='#' className="forgot-password">Forgot password?</a>
+                    
+                    <input type="submit" value="Login" className='login-btn' name="login-btn" id="login-btn" />
+                    
+                </div>
+
+                <div className="other-utils">
+                    <span className="signup-text">Dont Have Account ? <a href='#' className="signup-btn">Signup!</a></span>
+                </div>
             </div>
         </>
     )
